02eedad8773c2b47f28d01cbdfc761c4308aee7b
viliusDaug <viliusd29@gmail.com>
2024-03-12 13:13:59 +0200

    back button

--- app/src/main/java/com/example/viktorina/AboutActivity.java
+++ app/src/main/java/com/example/viktorina/AboutActivity.java
@@ -7,12 +7,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.card.MaterialCardView;
 
 public class AboutActivity extends AppCompatActivity {
 
     MaterialCardView developerCard, darkMode;
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class AboutActivity extends AppCompatActivity {
 
         developerCard=findViewById(R.id.developerCard);
         darkMode=findViewById(R.id.darkMode);
+        backButton=findViewById(R.id.back);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AboutActivity.this, MainActivity.class));
+            }
+        });
 
         developerCard.setOnClickListener(new View.OnClickListener() {
             @Override
